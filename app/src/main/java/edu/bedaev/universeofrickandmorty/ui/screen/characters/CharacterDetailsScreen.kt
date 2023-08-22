55d997d59f8c00ec03da9dc4c6701a8f00364dfe
Sergey Bedaev <serge_sargon@mail.ru>
2023-08-22 23:22:45 +1000

    parallax effect для изображения на экране деталей персонажа

--- app/src/main/java/edu/bedaev/universeofrickandmorty/ui/screen/characters/CharacterDetailsScreen.kt
+++ app/src/main/java/edu/bedaev/universeofrickandmorty/ui/screen/characters/CharacterDetailsScreen.kt
@@ -7,10 +7,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.Circle
@@ -23,9 +23,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -76,7 +80,39 @@ fun CharacterDetailsScreen(
         }
     ) { paddings ->
 
-        Column(
+        val paddingStart = dimensionResource(id = R.dimen.character_detail_start_padding)
+        val textColor = MaterialTheme.colorScheme.surface
+        val lazyState = rememberLazyListState()
+        val firstItemTranslationY by remember {
+            derivedStateOf {
+                when {
+                    lazyState.layoutInfo.visibleItemsInfo.isNotEmpty() &&
+                            lazyState.firstVisibleItemIndex == 0 ->
+                        lazyState.firstVisibleItemScrollOffset * .25f
+
+                    else -> 0f
+                }
+            }
+        }
+
+        val visibility by remember {
+            derivedStateOf {
+                when {
+                    lazyState.layoutInfo.visibleItemsInfo.isNotEmpty() && lazyState.firstVisibleItemIndex == 0 -> {
+                        val imageSize = lazyState.layoutInfo.visibleItemsInfo[0].size
+                        val scrollOffset = lazyState.firstVisibleItemScrollOffset
+
+                        scrollOffset / imageSize.toFloat()
+                    }
+
+                    else -> 1f
+                }
+            }
+        }
+
+
+        LazyColumn(
+            state = lazyState,
             modifier = Modifier
                 .fillMaxSize()
                 .padding(
@@ -86,81 +122,82 @@ fun CharacterDetailsScreen(
                 )
                 .background(color = MaterialTheme.colorScheme.inverseSurface)
         ) {
-            GlideImageWithPreview(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(256.dp)
-                    .padding(
-                        start = 4.dp, end = 4.dp
-                    ),
-                contentDescription = person.name,
-                model = person.image,
-                contentScale = ContentScale.Crop
-            )
-
-            val paddingStart = dimensionResource(id = R.dimen.character_detail_start_padding)
-            val textColor = MaterialTheme.colorScheme.surface
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = paddingStart),
-                text = person.name.uppercase(),
-                textAlign = TextAlign.Start,
-                color = textColor,
-                style = MaterialTheme.typography.titleLarge,
-            )
-            GradientDivider(
-                modifier = Modifier.padding(vertical = 4.dp),
-                dividerHeight = 3.dp
-            )
-
-            LazyColumn(
-                modifier = Modifier
-                    .padding(top = 8.dp)
-                    .fillMaxWidth()
-            ) {
-                item {
-                    PersonInfo(
-                        modifier = Modifier
-                            .padding(
-                                start = paddingStart,
-                                top = dimensionResource(id = R.dimen.vertical_space_between_text_medium),
-                                bottom = dimensionResource(id = R.dimen.vertical_space_between_text_big),
-                            )
-                            .fillMaxWidth(),
-                        textColor = textColor,
-                        person = person
-                    )
-                    GradientDivider(dividerHeight = 3.dp)
-                }
-                // episodes title
-                item {
-                    Text(
-                        modifier = Modifier.padding(
+            // image
+            item {
+                GlideImageWithPreview(
+                    modifier = Modifier
+                        .fillParentMaxWidth()
+                        .padding(
+                            start = 4.dp, end = 4.dp
+                        )
+                        .graphicsLayer {
+                            alpha = 1f - visibility
+                            translationY = firstItemTranslationY
+                        },
+                    contentDescription = "Header image",
+                    model = person.image,
+                    contentScale = ContentScale.Crop
+                )
+            }
+            item {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp, start = paddingStart),
+                    text = person.name.uppercase(),
+                    textAlign = TextAlign.Start,
+                    color = textColor,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                GradientDivider(
+                    modifier = Modifier.padding(vertical = 4.dp),
+                    dividerHeight = 3.dp
+                )
+                PersonInfo(
+                    modifier = Modifier
+                        .padding(
+                            start = paddingStart,
+                            top = dimensionResource(id = R.dimen.vertical_space_between_text_medium),
+                            bottom = dimensionResource(id = R.dimen.vertical_space_between_text_big),
+                        )
+                        .fillMaxWidth(),
+                    textColor = textColor,
+                    person = person
+                )
+                GradientDivider(dividerHeight = 3.dp)
+            }
+            // episodes title
+            item {
+                Text(
+                    modifier = Modifier
+                        .fillParentMaxWidth()
+                        .padding(
                             start = paddingStart,
                             top = dimensionResource(id = R.dimen.vertical_space_between_text_big),
                             bottom = dimensionResource(id = R.dimen.vertical_space_between_text_medium),
                         ),
-                        text = "${stringResource(id = R.string.episodes)}:",
-                        color = textColor,
-                        style = MaterialTheme.typography.titleLarge
-                    )
-                }
-                // episodes items
-                items(person.episodeList.size) { index ->
-                    val episode = person.episodeList[index]
-                    Text(
-                        modifier = Modifier.padding(
+                    text = "${stringResource(id = R.string.episodes)}:",
+                    color = textColor,
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+            // episodes items
+            items(person.episodeList.size) { index ->
+                val episode = person.episodeList[index]
+                Text(
+                    modifier = Modifier
+                        .fillParentMaxWidth()
+                        .padding(
                             start = paddingStart,
                             bottom = dimensionResource(id = R.dimen.vertical_space_between_text_medium)
                         ),
-                        text = episode,
-                        color = textColor,
-                        style = MaterialTheme.typography.bodyMedium
-                    )
-                }
+                    text = episode,
+                    color = textColor,
+                    style = MaterialTheme.typography.bodyMedium
+                )
             }
         }
+
     }
 }
 
